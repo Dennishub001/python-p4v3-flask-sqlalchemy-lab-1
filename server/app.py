2a9566007cc61987390f90550269cb7206a0ed7e
dennisochieng <ochiengdennis32@gmail.com>
2025-06-17 02:02:24 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +21,26 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def get_earthquake(id: int):
+    earthquake = db.session.get(Earthquake, id)
+    if earthquake:
+        return make_response(jsonify(earthquake.to_dict()), 200)
+    return make_response(jsonify({"message": "Earthquake 9999 not found."}), 404)
 
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def get_earthquakes_by_magnitude(magnitude: float):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    results = [eq.to_dict() for eq in earthquakes]
+    return make_response(
+        jsonify({
+            "count": len(results),
+            "quakes": results
+        }),
+        200
+    )
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
